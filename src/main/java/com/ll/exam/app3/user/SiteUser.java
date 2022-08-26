d81e2bc908e43d758eac8f0ee14cfc52cbb1a242
seeunla <107592967+seeunla@users.noreply.github.com>
2022-08-26 13:48:31 +0900

    세팅

---
+++ src/main/java/com/ll/exam/app3/user/SiteUser.java
@@ -0,0 +1,29 @@
+package com.ll.exam.app3.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+}
